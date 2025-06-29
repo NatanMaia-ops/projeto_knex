3d85cbc6bf54c39bee224a4f4f79dd003925fe08
Natan Alencar <natanmaia2005@gmail.com>
2025-06-29 07:39:36 -0300

    Documentacao e finalizacao do desafio

--- backend/src/main/java/com/knex/desafioknex/controller/DeputadoController.java
+++ backend/src/main/java/com/knex/desafioknex/controller/DeputadoController.java
@@ -1,6 +1,7 @@
 package com.knex.desafioknex.controller;
 import java.math.BigDecimal;
 import java.util.List;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.knex.desafioknex.dto.DeputadoDTO;
 import com.knex.desafioknex.dto.DespesaDTO;
 import com.knex.desafioknex.service.DeputadoService;
+import org.springframework.http.MediaType;
+
 
 @RestController
 @RequestMapping ("/api/deputado")
@@ -28,7 +31,7 @@ public class DeputadoController {
     }
 
 
-    @PostMapping ("/upload")
+    @PostMapping (value = "/upload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadCSV(@RequestParam("file") MultipartFile file) {
         try {
             deputadoService.uploadCSV(file);
@@ -38,20 +41,26 @@ public class DeputadoController {
         }
     }
 
+
     @GetMapping ("/findByUf")
     public List<DeputadoDTO> listarDeputadosPorUf(@RequestParam String uf) {
         return deputadoService.buscarDeputadosPorUf(uf);
     }
 
-    @GetMapping ("deputados/{id}/total-expenses")
+
+    @GetMapping (value = "deputados/{id}/total-expenses")
     public BigDecimal somatorioPorDeputado(@PathVariable Long id){
         return deputadoService.somatorioPorDeputado(id);
     }
+
+
 
     @GetMapping ("/sum-total-expenses")
     public BigDecimal somatorioTotalDespesas() {
         return deputadoService.somatorioTotalDespesas();
     }
+
+
 
     @GetMapping("/deputados/{id}/despesas")
     public List<DespesaDTO> listarDespesasPorDeputado(@PathVariable Long id) {
